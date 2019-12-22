e89dc5099c4da3fefedec6155ae061b35bd36a2b
Mateusz Burzyński <mateuszburzynski@gmail.com>
2019-12-22 22:47:18 +0100

    Update tests

--- packages/cache/__tests__/hydration.js
+++ packages/cache/__tests__/hydration.js
@@ -8,7 +8,7 @@ test('it works', () => {
   let hash = hashString(css)
   safeQuerySelector(
     'body'
-  ).innerHTML = `<style data-emotion-css="${hash}">.css-${hash}{${css}}</style>`
+  ).innerHTML = `<style data-emotion="css ${hash}">.css-${hash}{${css}}</style>`
   let cache = createCache()
   expect(cache.inserted).toEqual({ [hash]: true })
   expect(document.documentElement).toMatchSnapshot()
